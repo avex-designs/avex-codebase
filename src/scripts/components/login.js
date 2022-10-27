4bdbe38388430b7f7e86f323412b539b93d36e14
sanj718 <ssobirjonov@gmail.com>
2022-10-26 22:29:19 -0400

    scripts & minor edits & shopify cli3

---
+++ src/scripts/components/login.js
@@ -0,0 +1,13 @@
+class LoginComp extends HTMLElement {
+  constructor() {
+    super();
+    console.log("Started!!!!aa");
+  }
+
+  // setReady() {
+  //   this.setAttribute("ready", "");
+  // }
+}
+customElements.define("login-component", LoginComp);
+
+console.log("ssss");
