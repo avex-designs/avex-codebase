c1afe1d98ce0fde549d2a2c2c5f6b438e615dfef
Evgeniy <evgeniymuhamedjanov@gmail.com>
2022-11-23 03:06:51 +0500

    Fix JS variable names

--- src/scripts/components/html-fetcher.js
+++ src/scripts/components/html-fetcher.js
@@ -4,72 +4,69 @@ const loadingTypes = {
   LAZY: "lazy",
 };
 
-customElements.define(
-  elementName,
-  class extends HTMLElement {
-    #needsUpdate = false;
-    #url;
-    #abortController;
-    #loading = loadingTypes.EAGER;
-    #intersectionObserver;
+class HTMLFetcher extends HTMLElement {
+  #url;
+  #abortController;
+  #loading = loadingTypes.EAGER;
+  #intersectionObserver;
 
-    constructor() {
-      super();
-      this.#intersectionObserver = new IntersectionObserver(
-        (entries, observer) => {
-          if (!entries[0].isIntersecting) return;
-          observer.unobserve(this);
-          this.updateHTML();
-        },
-        {
-          rootMargin: "0px 0px 0px 0px",
-        }
-      );
-    }
-
-    connectedCallback() {
-      const loading = this.getAttribute("loading");
-      if (Object.values(loadingTypes).includes(loading)) {
-        this.#loading = loading;
-      }
-
-      const url = this.getAttribute("href");
-      if (!url) return;
-
-      this.setUrl(url);
-    }
-
-    setUrl(url) {
-      this.#url = url;
-      if (this.#loading === loadingTypes.EAGER) {
+  constructor() {
+    super();
+    this.#intersectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        if (!entries[0].isIntersecting) return;
+        observer.unobserve(this);
         this.updateHTML();
-        return;
+      },
+      {
+        rootMargin: "0px 0px 0px 0px",
       }
-      this.#intersectionObserver.observe(this);
+    );
+  }
+
+  connectedCallback() {
+    const loading = this.getAttribute("loading");
+    if (Object.values(loadingTypes).includes(loading)) {
+      this.#loading = loading;
     }
 
-    async updateHTML() {
-      if (this.#abortController) {
-        this.#abortController.abort();
-      }
-      this.#abortController = new AbortController();
-      try {
-        const response = await fetch(this.#url, {
-          signal: this.#abortController.signal,
-        });
-        if (!response.ok)
-          throw new Error(`Response error from the "${response.url}" URL`);
+    const url = this.getAttribute("href");
+    if (!url) return;
 
-        const html = await response.text();
-        const newdocument = new DOMParser().parseFromString(html, "text/html");
-        const $newElement = newdocument.querySelector(elementName);
-        if ($newElement) this.innerHTML = $newElement.innerHTML;
-        else this.innerHTML = html;
-      } catch (error) {
-        if (error.name !== "AbortError") {
-          throw error;
-        }
+    this.setUrl(url);
+  }
+
+  setUrl(url) {
+    this.#url = url;
+    if (this.#loading === loadingTypes.EAGER) {
+      this.updateHTML();
+      return;
+    }
+    this.#intersectionObserver.observe(this);
+  }
+
+  async updateHTML() {
+    if (this.#abortController) {
+      this.#abortController.abort();
+    }
+    this.#abortController = new AbortController();
+    try {
+      const response = await fetch(this.#url, {
+        signal: this.#abortController.signal,
+      });
+      if (!response.ok)
+        throw new Error(`Response error from the "${response.url}" URL`);
+
+      const html = await response.text();
+      const newdocument = new DOMParser().parseFromString(html, "text/html");
+      const $newElement = newdocument.querySelector(elementName);
+      if ($newElement) this.innerHTML = $newElement.innerHTML;
+      else this.innerHTML = html;
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        throw error;
       }
     }
   }
-);
+}
+customElements.define(elementName, HTMLFetcher);
