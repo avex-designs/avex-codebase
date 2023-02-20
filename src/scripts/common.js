6c03fd18b5e36d26921d213e9256686b4afe84f9
sanj718 <ssobirjonov@gmail.com>
2023-02-20 10:45:48 -0500

    commit 1

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -6,7 +6,6 @@ import "liquid-ajax-cart";
  */
 // move to theme.liquid refer to Kit&ace
 // configureCart("addToCartCssClass", "js-ajax-cart-opened");
-console.log("HEEEY");
 /**
  * Configure Swiper Modules
  */
